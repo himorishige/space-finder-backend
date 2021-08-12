45200e13732a584c00ee604562a7296317fb99e6
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-12 12:05:37 +0900

    section-4

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -4,6 +4,7 @@ import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/lib/aws-l
 import { join } from 'path';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/lib/aws-apigateway';
 import { GenericTable } from './GenericTable';
+import { NodejsFunction } from 'aws-cdk-lib/lib/aws-lambda-nodejs';
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, 'SpaceApi');
@@ -16,6 +17,11 @@ export class SpaceStack extends Stack {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main',
+    });
+
+    const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+      entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
+      handler: 'handler',
     });
 
     // Hello Api lambda Integration
