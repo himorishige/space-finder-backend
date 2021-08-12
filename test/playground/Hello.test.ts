f50ee28d9904b4b1d5793ec1f42a2ae79f8ee959
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-12 14:51:13 +0900

    add CR functions

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,3 +1,13 @@
-import { handler } from '../../services/node-lambda/hello';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { handler } from '../../services/SpacesTable/Read';
 
-handler({}, {});
+const event: APIGatewayProxyEvent = {
+  queryStringParameters: {
+    spaceId: '85a38cbd-4ffd-41c5-af4e-0ae797ed80bb',
+  },
+} as any;
+
+const result = handler(event, {} as any).then((apiResult) => {
+  const items = JSON.parse(apiResult.body);
+  console.log(123);
+});
